4b4c4e4e7b6a3633e4bd6c814d6feff3e66e8f14
Thein Naing <theinnaing0908@gmail.com>
2023-10-04 18:17:03 +0800

    Create passwordcracker.py

---
+++ passwordcracker.py
@@ -0,0 +1,15 @@
+import hashlib
+
+def crackhash(inputPass):
+    try:
+        passFile = open("password", "r")
+    except:
+        print("Could not find file")
+
+    for password in passFile:
+        encPass = password.encode("utf-8")
+        digest = hashlib.md5(encPass.strip()).hexdigest()
+        if digest == inputPass:
+            print("Password Found" + password)
+if __name__ == "__main__":
+        crackhash("1a1dc91c907325c69271ddf0c944bc72")
