90b606607a58d230b8da4b2b248f68e3dc2591f4
Thein Naing <theinnaing0908@gmail.com>
2023-10-04 18:38:06 +0800

    Update traffic.py

--- traffic.py
+++ traffic.py
@@ -1,9 +1,10 @@
 # Network Traffic Analysis from medium.com
+# return country/city name of the IP target
 
 import geoip2.database
 reader  = geoip2.database.Reader('GeoLite2-City.mmdb')
 response = reader.city('128.101.101.101')
-print(response.city.name)
+# print(response.city.name) 
 
 
 import geoip2.database
