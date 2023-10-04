9528e2795f7801e286a3de5d5b9f1acbd7c0b4f6
Thein Naing <theinnaing0908@gmail.com>
2023-10-04 17:45:47 +0800

    Create traffic.py

---
+++ traffic.py
@@ -0,0 +1,12 @@
+import geoip2.database
+reader  = geoip2.database.Reader('GeoLite2-City.mmdb')
+response = reader.city('128.101.101.101')
+print(response.city.name)
+
+
+import geoip2.database
+reader  = geoip2.database.Reader('GeoLite2-City.mmdb')
+response = reader.country('128.101.101.101')
+print(response.country.name)
+
+
