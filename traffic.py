1b623f6bcca413d7ef867b48418acf922aba8fd8
Thein Naing <theinnaing0908@gmail.com>
2023-10-04 18:48:28 +0800

    Update traffic.py

--- traffic.py
+++ traffic.py
@@ -4,7 +4,7 @@
 import geoip2.database
 reader  = geoip2.database.Reader('GeoLite2-City.mmdb')
 response = reader.city('128.101.101.101')
-# print(response.city.name) 
+print(response.city.name) 
 
 
 import geoip2.database
